bdd7ceffdcfcce09bba05d4641c2a79b5d596c44
Uralegend <bcgraham917@gmail.com>
2024-03-25 16:02:48 -0400

    1. made sure to put them in parentheses to make sure the sum of the 3 is divided by 3.0f 2. corrected to store input in n3

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	// made sure to put them in parentheses to make sure the sum of the 3 is divided by 3.0f
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	// corrected to store input in n3
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
